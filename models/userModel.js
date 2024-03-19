82bfd3d79d7a3acf18ded5f4a42da006bd8757af
MESSELMII_YAHYA <107892703+MESSELMIyahya@users.noreply.github.com>
2024-03-19 05:57:25 +0000

    Fix userSchema's provider field's enum array

--- models/userModel.js
+++ models/userModel.js
@@ -64,7 +64,7 @@ const userSchema = new mongoose.Schema(
       provider: {
         required: true,
         type: String,
-        enum: ['google', 'enum']
+        enum: ['google', 'email']
       }
     }
   },
