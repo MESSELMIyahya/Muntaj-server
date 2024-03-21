b0469a437ce9e25e9b1bb8c3bb889bc6c584db85
MESSELMII_YAHYA <107892703+MESSELMIyahya@users.noreply.github.com>
2024-03-21 16:49:10 +0000

    Change the is-authenticatd controller to return the user data if it's exists,and remove the verification process from it because the middleware will do all of this

--- controllers/auth/IsAuthenticated/index.js
+++ controllers/auth/IsAuthenticated/index.js
@@ -1,21 +1,11 @@
 const { verifyAccessToken,verifyRefreshToken } = require('../../../utils/auth/jwt/index');
 
 
-
 // is  Authenticated  
-
 const AuthIsAuthenticatedController = async(req,res) => {
-    // verify if cookies exist 
-    const cos = req.cookies ;
-
-    if(!cos?.ac_to||!cos?.re_to) return res.status(401).json({authenticated:false});
-    try{
-        // verify access token and refresh token
-        await verifyRefreshToken(cos?.re_to) ;
-        const payload = await verifyAccessToken(cos?.ac_to);
-
-        return res.status(200).json({authenticated:true,user:payload})
-    }catch(err){
+    if(req.user){
+        return res.status(200).json({authenticated:true,user:req.user})
+    }else {
         return res.status(401).json({authenticated:false})
     }
 }
